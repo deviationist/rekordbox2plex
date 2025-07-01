4bb1a7a8ed6f84aec571d7bb11c9fe465f3636eb
Robert Sæther <robert4832@pm.me>
2025-07-01 14:12:33 +0000

    Added WIP python project

---
+++ main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+from dotenv import load_dotenv
+load_dotenv()  # Load environment variables from .env
+
+import json
+from rekordbox_file_resolver import resolve_track
+
+def main():
+  file_path = "/data/music/main/Minimal but pling plong/Cajal - 0220 (Original Mix).aiff"
+  print(f"Resolving file: {file_path}")
+  track = resolve_track(file_path)
+  if track:
+      print("File resolved successfully:", json.dumps(track, indent=2))
+  else:
+      print("File could not be resolved.")  
+
+if __name__ == "__main__":
+    main()
